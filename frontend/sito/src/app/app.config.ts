733f17a0c7592647e14ab9184b05383d9b5a1eaa
abdystudies <ibrahim.abdelrahman@iisgalvanimi.edu.it>
2026-01-11 18:38:53 +0000

    firebase setup

---
+++ frontend/sito/src/app/app.config.ts
@@ -0,0 +1,24 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, initializeApp,  } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+import { routes } from './app.routes';
+import { environment } from '../environment';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+
+    provideAuth(() => getAuth()),
+
+    provideFirestore(() => getFirestore())
+    
+
+  ]
+};
---
+++ frontend/sito/src/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    firebase: {
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: ""
+    }
+}
