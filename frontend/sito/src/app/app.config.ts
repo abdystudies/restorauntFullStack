faf3fe4c74f76db1e5091b57331d04d80aa4abdb
abdystudies <ibrahim.abdelrahman@iisgalvanimi.edu.it>
2026-01-12 10:29:12 +0000

    ho sborrato

--- frontend/sito/src/app/app.config.ts
+++ frontend/sito/src/app/app.config.ts
@@ -5,7 +5,7 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { environment } from '../environment';
+import { environment } from './environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
