3c26eb6d42876a4d7a158375fb6d7a38a3c50445
abdystudies <ibrahim.abdelrahman@iisgalvanimi.edu.it>
2026-01-12 09:56:17 +0000

    setupping login and register

--- frontend/sito/src/app/app.config.ts
+++ frontend/sito/src/app/app.config.ts
@@ -1,24 +1,22 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideFirebaseApp, initializeApp,  } from '@angular/fire/app';
-import { provideAuth, getAuth } from '@angular/fire/auth';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
 
+    //inizializza firebase con la config in environment
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-
+    //ci rende disponibile l'auth di firebase
     provideAuth(() => getAuth()),
-
+    //ci rende disponibile firestore il db che vedremo
     provideFirestore(() => getFirestore())
-    
-
   ]
 };
--- frontend/sito/src/environment.ts
+++ frontend/sito/src/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     firebase: {
-        apiKey: "",
-        authDomain: "",
-        databaseURL: "",
-        projectId: "",
-        storageBucket: "",
-        messagingSenderId: "",
-        appId: "",
-        measurementId: ""
+        apiKey: "AIzaSyDQ3ffqiWapdCgmb36wv77mv_fvuCnDNQ0",
+        authDomain: "prova-7fad7.firebaseapp.com",
+        databaseURL: "https://prova-7fad7-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "prova-7fad7",
+        storageBucket: "prova-7fad7.firebasestorage.app",
+        messagingSenderId: "1022048579514",
+        appId: "1:1022048579514:web:b6beb1a04305d4ec5d3783",
+        measurementId: "G-J1FM3TNCQ6"
     }
 }
